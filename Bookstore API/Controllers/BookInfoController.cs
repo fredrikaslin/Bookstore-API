de523468f0493b04710d0a18db7510d18e1ecea7
Fredrik Åslin <fredrik.aslin@voyado.com>
2021-01-14 18:36:55 +0100

    update controller, add cshtml page

--- Bookstore API/Controllers/BookInfoController.cs
+++ Bookstore API/Controllers/BookInfoController.cs
@@ -1,17 +1,21 @@
 ﻿using Bookstore_API.Models;
 using Bookstore_API.Repository;
 using System.Web.Http;
+using System.Web.Mvc;
 
 namespace Bookstore_API.Controllers
 {
     public class BookInfoController : ApiController
     {
         private LibraryRepository library = new LibraryRepository();
+
+
         [HttpGet]
-        public BookModel SearchBook(string ISBN)
+        public ActionResult SearchBook(string ISBN)
         {
             var book = library.GetBook(ISBN);
             return book;
+
         }
     }
 }
